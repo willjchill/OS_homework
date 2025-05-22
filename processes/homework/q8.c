0f670a375bcc977d37abcc4da2d0092aa874a14a
William <willjdesueza@gmail.com>
2025-05-22 15:47:34 -0400

    q8 for ch5 complete

--- processes/homework/q8.c
+++ processes/homework/q8.c
@@ -3,18 +3,31 @@
 #include <stdio.h>
 #include <sys/types.h>
 
+#define FORK_ERROR(rc) do { if(rc < 0) { fprintf(stderr, "Cannot fork."); exit(1); } } while(0)
+
 int main(int argc, char * argv[]) {
 	int pipefd[2];
-	pipe(pipefd);
-	int rc = fork();
-	if (rc < 0) {
-		fprintf(stderr, "Cannot fork");
+	if(pipe(pipefd) == -1) {
+		fprintf(stderr, "Cannot pipe.");
 		exit(1);
 	}
-	if (rc == 0) {
+	int rc = fork();
+	FORK_ERROR(rc);
+	if (rc == 0) { //first child
+		close(pipefd[0]); // first child will be writing
+		dup2(pipefd[1], STDOUT_FILENO);
+		printf("hello - sent from first child");	
+	}
+	else {
 		int rc_2 = fork();
-		pid_t child_pid = getpid();
-		printf("Child PID: %d", child_pid);
+		FORK_ERROR(rc_2);
+		dup2(pipefd[0], STDIN_FILENO);
+		close(pipefd[1]); // first child will be reading
+		if(rc_2 == 0) { // second child
+			char buffer[100];
+			read(pipefd[0], buffer, 100); // possible vulnerability but wtv lol  
+			printf("second child sees - %s\n", buffer);	
+		}
 	}
 	return 0;
 }
